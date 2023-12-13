87c8c985d82f41dee5e34174d67670d509c4206a
U-BSD\504685 <504685@SHS-15W2NW3.beaverton.k12.or.us>
2023-12-13 11:47:39 -0800

    first 1

---
+++ Node.h
@@ -0,0 +1,17 @@
+//Header file for Node class
+#include "Student.h"
+
+using namespace std;
+
+class Node
+{
+	public:
+		Node(Student*); //Constructor
+		~Node(); //Destructor
+		Student* getStudent(); //Get Student pointer
+		void setNext(Node*); //Set the next pointer to the corresponding node pointer
+		Node* getNext(); //Get next Node pointer
+	protected:
+		Student* value;
+		Node* next;
+};
