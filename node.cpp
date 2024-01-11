eb36d42604b71bc1ddcf22c9ef4326a4b7eeb471
Mesay T <mesayte@Mesays-iMac-3.local>
2024-01-10 20:26:08 -0800

    2 TWO COMMITS AT THE SAME TIME RAR

---
+++ node.cpp
@@ -0,0 +1,32 @@
+ // node .cpp from peers 
+//Node class: each node holds a student and next student (links to the next student)
+#include "Node.h"
+#ifndef STUDENT_H
+#define STUDENT_H
+
+Node::Node(Student* studentvalue)
+{
+	value = studentvalue;
+	next = NULL;
+}
+
+Node::~Node()
+{
+	next = NULL;
+}
+
+Student* Node::getStudent()
+{
+	return value;
+}
+
+void Node::setNext(Node* newnext)
+{
+	next = newnext;
+}
+
+Node* Node::getNext()
+{
+	return next;
+}
+#endif
