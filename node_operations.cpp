1114039bee32e9440183027bf250a2e4b111d744
Mesay T <mesayte@Mesays-iMac-3.local>
2024-01-12 20:04:50 -0800

    second commit we up gang

---
+++ node_operations.cpp
@@ -0,0 +1,26 @@
+#include "Node.h"
+// Implementation of Node class functions
+Node::Node(Student* studentvalue)
+{
+    value = studentvalue;
+    next = nullptr;
+}
+Node::~Node()
+{
+    next = nullptr;
+}
+Student* Node::getStudent()
+{
+    return value;
+}
+void Node::setNext(Node* newnext)
+{
+    next = newnext;
+}
+Node* Node::getNext()
+{
+    return next;
+}
+
+
+
